5aa156df9fe8e3db8b1a76562a0dd1084a440954
Samolivyu Manki <samolivyumanki@outlook.com>
2024-04-19 12:32:47 +0300

    App.js edit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
 
-
 const App = () => {
   return (
     <Routes>
-      </Route path= '/home' element= {<Home/>} >
+      <Route path='/src/Home.jsx' element={<Home />} />
     </Routes>
   );
 };
