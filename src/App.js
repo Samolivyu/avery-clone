b0d765c0647e7fe2c965a640b5f33e534c682308
Samolivyu Manki <samolivyumanki@outlook.com>
2024-05-08 14:05:45 +0300

    debugging

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
-
 
 const App = () => {
   return (
-    <Routes>
-      </Route path= '/home' element= {<Home/>} >
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 };
 
